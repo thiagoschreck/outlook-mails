8148dd76d4a3b566a68dffd36b2d9de2c263e186
Thiago Schreck <56611782+thiagoschreck@users.noreply.github.com>
2020-04-08 11:39:47 -0300

    Initial upload

---
+++ autoemail.ps1
@@ -0,0 +1,18 @@
+﻿# -- Constants -- #
+$olFormatUnspecified = 0
+$olFormatPlain = 1
+$olFormatHTML = 2
+$olFormatRichText = 3
+
+$Outlook = New-Object -ComObject Outlook.Application
+$Mail = $Outlook.CreateItem(0)
+$Mail.To = "Receiver"
+$Mail.CC = "Carbon copy"
+$Mail.Subject = "Subject"
+$Mail.Bodyformat = $olFormatHTML
+$Mail.HTMLBody = @"
+	Body (formatted as HTML)
+"@
+
+$Mail.Send()
+Out-Null
